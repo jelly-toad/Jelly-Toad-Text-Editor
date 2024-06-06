601d872af5d338aa4ddfe96afb3063f3d0af2e98
Seamus Sullivan <seamusmsullivan23@gmail.com>
2024-06-06 15:04:07 -0400

    Basic staginh

---
+++ src/core/JT_Window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ssullivan on 6/6/2024.
+//
+
+#include "JT_Window.h"
---
+++ src/core/JT_Window.h
@@ -0,0 +1,18 @@
+//
+// Created by ssullivan on 6/6/2024.
+//
+
+#ifndef JT_WINDOW_H
+#define JT_WINDOW_H
+
+
+
+class JT_Window {
+
+
+
+};
+
+
+
+#endif //JT_WINDOW_H
